54da036460bdca67ed7e9d1ee1a2e062c5cf736b
pedro <4pedro.facci@gmail.com>
2026-05-06 22:10:31 -0300

    arquivo de conexão alterada para uso de variáveis como parametro, validado funcionamento da função de adicionar conta no banco de dados

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -6,10 +6,17 @@ import java.sql.SQLException;
 
 public class ConnectionFactory { //NOSSO ARQUIVO DE CONEXÃO COM O BANCO DE DADOS
 
+
+    //"jdbc:postgresql://localhost:5432/byte_bank?user=postgres&password=abacaxiHot"
+
+    String url = "jdbc:postgresql://localhost:5432/byte_bank?";
+    String user = "postgres";
+    String password = "abacaxiHot";
+
     public Connection recuperarConexao(){
         try {
             return DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/byte_bank?user=postgres&password=abacaxiHot");
+                    .getConnection(url, user, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
